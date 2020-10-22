c11c1e533199a41e53de5b41024ab1ab9df99307
SAhuja789 <70078744+SAhuja789@users.noreply.github.com>
2020-10-22 14:17:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("Link for Tutorial: https://www.youtube.com/watch?v=rgUp302f_lY");
+
+// This script has no purpose at this moment. You may edit it if you are using it as your main script
