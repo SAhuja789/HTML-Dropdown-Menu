d3012ef0020acba96f7feccd60a54c9f2649666b
SAhuja789 <70078744+SAhuja789@users.noreply.github.com>
2020-10-22 14:18:19 -0400

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-console.log("Link for Tutorial: https://www.youtube.com/watch?v=rgUp302f_lY");
-
-// This script has no purpose at this moment. You may edit it if you are using it as your main script
